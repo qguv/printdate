d94cc5f537f7f75e9e6beb08aa487ac488274361
Quint Guvernator <quint@guvernator.net>
2024-05-26 22:19:48 +0200

    doesnt work

--- print.py
+++ print.py
@@ -4,19 +4,70 @@
 
 import datetime
 import socket
+import struct
+import math
 
+import packbits
 from PIL import Image, ImageDraw, ImageFont
 
 HOST = "10.10.10.31"
 PORT = 9100
 text = datetime.date.today().isoformat()
+tape_width = 12  # mm
 
-label_height = 64
+def encode_raster_transfer(data, label_height):
+    """ Encode 1 bit per pixel image data for transfer over serial to the printer """
+    buf = bytearray()
+
+    # Send in chunks of 1 line (128px @ 1bpp = 16 bytes)
+    # This mirrors the official app from Brother. Other values haven't been tested.
+    chunk_size = math.ceil(label_height / 8)
+
+    for i in range(0, len(data), chunk_size):
+        print("chunk", i)
+        chunk = data[i : i + chunk_size]
+
+        # Encode as tiff
+        packed_chunk = packbits.encode(chunk)
+
+        # Write header
+        buf.extend(TRANSFER_COMMAND)
+
+        # Write number of bytes to transfer (n1 + n2*256)
+        length = len(packed_chunk)
+        buf.extend(unsigned_char.pack( int(length % 256) ))
+        buf.extend(unsigned_char.pack( int(length / 256) ))
+
+        # Write data
+        buf.extend(packed_chunk)
+
+    return buf
+
+TRANSFER_COMMAND = b"\x47"
+unsigned_char = struct.Struct('B')
+
+label_height = {
+  3.5: 24,
+  6: 42,
+  9: 64,
+  12: 84,
+  18: 128,
+  24: 170,
+}[tape_width]
+
+printable_height = {
+  3.5: 24,
+  6: 32,
+  9: 50,
+  12: 70,
+  18: 112,
+  24: 128,
+}
 
 text_size = 24
 text_border_width = 10
 
-label_length = int(2 * text_border_width + ((len(text)) * (text_size) * 0.6))
+label_length = int(2 * text_border_width + len(text) * text_size * 0.6)
 
 label = Image.new('1', (label_length, label_height), color = 1)
 
@@ -28,8 +79,13 @@ label = label.rotate(270, expand=1)
 
 image_data = bytearray(label.tobytes())
 
+label.save('image.jpg')  # DEBUG
+with open('image_data', 'w') as f:  # DEBUG
+    f.buffer.write(image_data)  # DEBUG
+
+# invert colors
 for i in range(len(image_data)):
-  image_data[i] = -image_data[i] + 255 # invert colors
+  image_data[i] = 255 - image_data[i]
 
 data = b'\x00' * 100
 data += b'\x1B\x40' # print start
@@ -37,17 +93,12 @@ data += b'\x1B\x69\x4D\x40' # pre-cut
 data += b'\x1B\x69\x4B\x08' # end cut
 data += b'\x4d\x02' # compression
 
-for line in range(label_length):
-    data_start = int(line * (label_height / 8))
-    data_end = int(data_start + (label_height / 8) - 1)
-    lower_padding = int((16 - label_height / 8) / 2)
-    upper_padding = int((16 - label_height / 8) - lower_padding)
-    image_part = image_data[data_start:data_end+1] # cut one line of the image
-    binary = bin(int(image_part.hex(), 16))[2:].zfill(64) # bytes to binary str
-    reversed = int(binary[::-1], 2).to_bytes(len(binary) // 8, byteorder='big') # reverse bit order and convert back to bytes
-    data_part = b'\x47\x11\x00\x0F' + b'\x00' * lower_padding + reversed + b'\x00' * upper_padding # line data for printer
-    data += data_part
+data += encode_raster_transfer(data, label_height)
+
 data += b'\x1A' # print end
+
+with open('sent', 'w') as f:  # DEBUG
+    f.buffer.write(data)  # DEBUG
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
